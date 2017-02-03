9d511b6fffbc90be3c332f88ac6268dff0e86b4c
Sergey <sergey@MacBook-Air-Sergey.local>
2017-02-03 12:50:33 -0800

    Sign up validation update

--- src/app/containers/Login/SignUp/index.js
+++ src/app/containers/Login/SignUp/index.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 import { css } from 'aphrodite';
 import { TextField, RaisedButton } from 'material-ui';
 import { mainAppStyles as S } from './style';
+import { loginCaptions } from '../../../utils/textCaptions';
+import { fieldValidate, EMAIL_REGEXP, PASSWORD_REGEXP } from '../../../utils/regExp';
 
 
 class SignUp extends React.Component {
@@ -10,37 +12,49 @@ class SignUp extends React.Component {
     login: '',
     password: '',
     confirmPassword: '',
+    errors: {
+      login: false,
+      password: false,
+      confirmPassword: false,
+    }
   };
 
   render() {
-    let { login, password, confirmPassword } = this.state;
+    let { login, password, confirmPassword, errors } = this.state;
 
     return (
       <div className={css(S.loginCard)}>
         <p className={css(S.loginCardLinks)}>
-          You can create an account or
+          { loginCaptions.createAccount }
           <Link to='/login/signIn'>
             <span className={css(S.loginLink)}>Login</span>
           </Link>
         </p>
+
         <TextField
+          errorText={errors.login && loginCaptions.emailIncorrect}
+          onBlur={() => this._onFieldBlur('login', EMAIL_REGEXP)}
           className={css(S.loginCardField)}
           underlineFocusStyle={{borderColor: '#45a7b9'}}
           onChange={this._onFieldChange('login')}
           value={login}
           hintText='Login'/>
         <TextField
+          errorText={errors.password && loginCaptions.passwordIncorrect}
+          onBlur={() => this._onFieldBlur('password', PASSWORD_REGEXP)}
           className={css(S.loginCardField)}
           underlineFocusStyle={{borderColor: '#45a7b9'}}
           onChange={this._onFieldChange('password')}
           value={password}
           hintText='Password'/>
         <TextField
+          onBlur={() => this._onFieldBlur()}
           className={css(S.loginCardField)}
           underlineFocusStyle={{borderColor: '#45a7b9'}}
           onChange={this._onFieldChange('confirmPassword')}
           value={confirmPassword}
           hintText='Confirm password'/>
+
         <RaisedButton
           onClick={this.onValidate}
           backgroundColor={'#45a7b9'}
@@ -51,12 +65,18 @@ class SignUp extends React.Component {
     )
   }
 
-  onValidate = () => {
-    let { login } = this.state;
 
-    var ck_email = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
-    console.log(ck_email.test(login));
+  _onFieldBlur = (errorField, regExpType) => {
+    this.setState(state => {
+      return {
+        errors: {
+          ...state.errors,
+          [errorField]: !fieldValidate(state[errorField], regExpType)
+        }
+      }
+    });
   };
+
 
   _onFieldChange = field => event => {
     this.setState({
---
+++ src/app/utils/textCaptions/index.js
@@ -0,0 +1,5 @@
+export const loginCaptions = {
+  createAccount: 'You can create an account or',
+  emailIncorrect: 'Email address is incorrect or to short',
+  passwordIncorrect: 'Password field should content at least 6 characters',
+};
